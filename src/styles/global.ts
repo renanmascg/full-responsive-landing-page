4b186913cc11040b0545b9838a45aee7496fbd6b
Renan Mascarenhas Goncalves <renan.goncalves@bv.com.br>
2020-11-23 21:37:56 -0300

    Adiciona estilização global e estiliza a Navbar utilizando styled components

---
+++ src/styles/global.ts
@@ -0,0 +1,31 @@
+import { createGlobalStyle} from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0
+  }
+  body {
+    background: #F4F6F9;
+    color: #3a3a3a;
+    -webkit-font-smoothing: antialiased;
+  }
+  body, input, button {
+    font-family: 'Poppins', sans-serif;
+    font-size: 16px;
+  }
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 500;
+  }
+  button {
+    cursor: pointer;
+  }
+  .toast-signin {
+    border-radius: 5px;
+    font-family: 'Poppins', sans-serif;
+    font-size: 16px;
+    font-weight: 500;
+  }
+`;
