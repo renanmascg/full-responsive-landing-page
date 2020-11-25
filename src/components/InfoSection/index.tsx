1444a130932a3c8e68571ed2c084ab8aa8205e55
Renan Mascarenhas Goncalves <renan.goncalves@bv.com.br>
2020-11-25 14:35:16 -0300

    Adiciona info section reutilizável dentro do projeto, adicionando flexibilidade ao codigo

---
+++ src/components/InfoSection/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Button } from '../ButtonElement';
+import { IInfoObjDTO } from '../../core/IInfoObjDTO';
+import { BtnWrap, Column1,Column2, Heading, Img, ImgWrap, InfoContainer, InfoRow, InfoWrapper, Subtitle, TextWrapper, TopLine } from './InfoSectionElements';
+
+
+
+interface IInfoSection {
+  obj: IInfoObjDTO;
+}
+
+const InfoSection: React.FC<IInfoSection> = ({ obj }) => {
+  return (
+    <>
+      <InfoContainer lightBg={obj.lightBg} id={obj.id}>
+        <InfoWrapper>
+          <InfoRow imgStart={obj.imgStart}>
+            <Column1>
+              <TextWrapper>
+                <TopLine>{obj.topLine}</TopLine>
+                <Heading lightText={obj.lightText}>{ obj.headline}</Heading>
+                <Subtitle darkText={obj.darkText}>{ obj.description}</Subtitle>
+                <BtnWrap>
+                  <Button 
+                    to="home"  
+                    smooth={true}
+                    duration={500}
+                    primary={obj.primary} 
+                    offset={-80}
+                    dark={obj.dark}> {obj.buttonLabel}</Button>
+                </BtnWrap>
+              </TextWrapper>
+            </Column1>
+            <Column2>
+              <ImgWrap>
+                <Img src={obj.img} alt={obj.altTag}/>
+              </ImgWrap>
+            </Column2>
+          </InfoRow>
+        </InfoWrapper>
+      </InfoContainer>
+    </>
+  )
+}
+
+export default InfoSection;
