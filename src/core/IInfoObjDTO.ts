82bae20da90fb2d527db84553c10be6e5183903e
Renan Mascarenhas Goncalves <renan.goncalves@bv.com.br>
2020-11-25 13:45:15 -0300

    Adiciona DTO utilizado para renderizar cade trecho da pagina

---
+++ src/core/IInfoObjDTO.ts
@@ -0,0 +1,16 @@
+export interface IInfoObjDTO {
+  id: String;
+  lightBg: Boolean;
+  lightText: Boolean;
+  lightTextDes: Boolean;
+  topLine: String;
+  headline: String;
+  description: String;
+  buttonLabel: String;
+  imgStart: Boolean;
+  img: any;
+  altTag: String;
+  dark: Boolean;
+  primary: Boolean;
+  darkText: Boolean;
+}
